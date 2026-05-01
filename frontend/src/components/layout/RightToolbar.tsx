3157b47dd4ec4ac6d0339d6f81c28631ea1ae1df
DUCANHUIT37 <ducanh47leloi@gmail.com>
2026-05-01 17:10:44 +0700

    fix[frontend]: right tool bar

--- frontend/src/components/layout/RightToolbar.tsx
+++ frontend/src/components/layout/RightToolbar.tsx
@@ -2,11 +2,18 @@ import { useModal } from "../../context/ModalContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGear, faEnvelope, faBell, faMessage } from "@fortawesome/free-solid-svg-icons";
 
+
 export default function RightToolbar() {
   const { openModal } = useModal();
+  const user = null;
+  const avatarUrl = user?.avatarUrl;
+  const displayName = user?.displayname ?? "User";
 
   return (
     <aside className="right-toolbar">
+      <div className="avt-container overflow-hidden">
+        <img src="https://2sao.vietnamnetjsc.vn/2016/07/18/10/04/7041969500357457.jpg" alt="avt" className="object-cover w-full h-full"/>
+      </div>
       <button className="tool-btn" onClick={() => openModal("settings")}>
         <FontAwesomeIcon icon={faGear} /> 
       </button>
@@ -16,7 +23,6 @@ export default function RightToolbar() {
       <button className="tool-btn" onClick={() => openModal("notifications")}>
       <FontAwesomeIcon icon={faBell} />
       </button>
-      <button className="tool-btn">EN</button>
     </aside>
   );
 }
