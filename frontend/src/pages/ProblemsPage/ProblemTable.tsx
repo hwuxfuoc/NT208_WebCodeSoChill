17d8b9fafba3749955b40787832a4e86ac0abb02
DUCANHUIT37 <ducanh47leloi@gmail.com>
2026-07-30 13:29:17 +0700

    Revert "fix[fe]: error of import path"
    This reverts commit 592e40ef0ad1d250df239d5d290c68e60ac7761d.

--- frontend/src/pages/ProblemsPage/ProblemTable.tsx
+++ frontend/src/pages/ProblemsPage/ProblemTable.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import type { Problem } from "../../types/problem";
+import type { Problem } from "../../services/problemService";
 
 interface ProblemTableProps {
   rows: Problem[];
